8bb49de4ee821cf2b7b9a0e4cecfe8ee63c2e51b
Kazi Tania <kazitania.reg@gmail.com>
2021-02-07 21:30:44 +0600

    code not done

--- hungry-monster.js
+++ hungry-monster.js
@@ -6,7 +6,6 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
 const displayMeals = meals =>{
     const mealsDiv = document.getElementById('meals');
     meals.forEach(meal =>{
-        // const meal = meals[i];
         const mealDiv = document.createElement('div');
         mealDiv.className ='meal';
         const mealInfo =`
@@ -14,35 +13,9 @@ const displayMeals = meals =>{
             <p>${meal.ingrideant}</p>;
             <button onclick="displayMealDetail('${meal.name}')">;
         `
-
         mealDiv.innerHTML = mealInfo;
-
         mealsDiv.appendChild(mealDiv);
     });
 }
-
 const displayMealDetail = name =>{
-
 }
-    // for (let i = 0; i < meals.length; i++) {
-    //     const meal = meals[i];
-        // const mealDiv = document.createElement('div');
-        // const name = document.createElement('h3');
-        // name.innerText = meal.name;
-        // const ingrediant = document.createElement('p');
-        // ingrediant.innerText = meal.ingrediant;
-        // mealDiv.appendChild(name);
-        // mealDiv.appendChild(ingrediant);
-
-
-    //     mealDiv.className = 'meal';
-    //     const mealInfo =`
-    //         <h3 class ="meal-name">${meal.name}</h3>
-    //         <p>${meal.ingrideant}</p>;
-    //     `
-    //     mealDiv.innerHTML = mealInfo;
-
-    //     mealsDiv.appendChild(mealDiv);
-    // }
-
-// }
