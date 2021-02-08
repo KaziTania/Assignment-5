f6012b1018c25b4b9a088675a127cd0287cece8e
Kazi Tania <kazitania.reg@gmail.com>
2021-02-08 23:26:01 +0600

    image done

--- hungry-monster.js
+++ hungry-monster.js
@@ -1,21 +1,41 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res => res.json())
-.then(data =>displayMeals(data));
-
-
+function searchData(){
+    let searchText = document.getElementById("search-text").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayMeals(data.meals))
+}
+function showIngridiant(mealId){
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayMealDetail(data.meals))   
+}
 const displayMeals = meals =>{
     const mealsDiv = document.getElementById('meals');
     meals.forEach(meal =>{
         const mealDiv = document.createElement('div');
         mealDiv.className ='meal';
+        mealDiv.id = "mealDivId"+ meal.idMeal
         const mealInfo =`
-            <h3 class ="meal-name">${meal.name}</h3>;
-            <p>${meal.ingrideant}</p>;
-            <button onclick="displayMealDetail('${meal.name}')">;
+            <img class="meal-img" src="${meal.strMealThumb}">
+            <h4 class ="meal-name">${meal.strMeal}</h4>
+            <button onclick="showIngridiant('${meal.idMeal}')" type="button" class="btn btn-danger">Ingridiant</button>
         `
         mealDiv.innerHTML = mealInfo;
         mealsDiv.appendChild(mealDiv);
     });
 }
-const displayMealDetail = name =>{
+const displayMealDetail = meals =>{
+    const mealdetailDiv = document.createElement('div');
+    const mealsDiv = document.getElementById('meals');
+    meals.forEach(meal =>{
+        const mealDiv = document.createElement('div');
+        mealDiv.className ='meal';
+        const mealInfo =`
+            <h4 class ="meal-name">${meal.strMeal}tania</h4>;
+        `
+        mealDiv.innerHTML = mealInfo;
+        mealsDiv.appendChild(mealDiv);
+    });
 }
